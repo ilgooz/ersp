ff28dca6ee5bd7cf2e0d33b660cff57f31ae1e66
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2015-09-16 15:07:20 +0300

    pretty err messages support

--- ersp.go
+++ ersp.go
@@ -3,18 +3,29 @@ package ersp
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 // add Must() ?
 
 var (
-	UnvalidFormData = "Unvalid Form Data"
-	BadFormEncoding = "Bad Form Encoding"
+	UnvalidFormData  = "Unvalid Form Data"
+	BadFormEncoding  = "Bad Form Encoding"
+	PrettyErrorTitle = "Error"
+)
+
+var (
+	MustBeANumberErr               = "must be a number"
+	MustBeNumbersErr               = "must be numbers"
+	MustBeCommaSeparatedNumbersErr = "must be comma separated numbers"
+	MustBeTrueOrFalseErr           = "must be true or false"
+	MustBeUTCErr                   = "must be UTC"
 )
 
 type Response struct {
 	Error Error `json:"error,omitempty"`
 	w     http.ResponseWriter
+	r     *http.Request
 }
 
 type Error struct {
@@ -22,11 +33,16 @@ type Error struct {
 	Fields  map[string]string `json:"fields,omitempty"`
 }
 
-func New(w http.ResponseWriter) *Response {
+func New(w http.ResponseWriter, r *http.Request) *Response {
 	return &Response{
 		w:     w,
+		r:     r,
 		Error: Error{Fields: make(map[string]string, 0)},
 	}
+}
+
+func (er *Response) isPretty() bool {
+	return er.r.Header.Get("X-Pretty-Error") == "true"
 }
 
 func (er *Response) Message(message string) *Response {
@@ -41,7 +57,32 @@ func (er *Response) Field(name, message string) *Response {
 
 func (er *Response) Send(status int) error {
 	if er.Error.Message == "" && len(er.Error.Fields) > 0 {
-		er.Message(UnvalidFormData)
+		if er.isPretty() {
+			er.Message(PrettyErrorTitle)
+		} else {
+			er.Message(UnvalidFormData)
+		}
+	}
+
+	if er.isPretty() {
+		fields := map[string]string{}
+
+		for key, val := range er.Error.Fields {
+			newKey := strings.Title(strings.Replace(key, "_", " ", -1))
+
+			var newVal string
+			switch val {
+			case MustBeANumberErr, MustBeNumbersErr, MustBeCommaSeparatedNumbersErr,
+				MustBeTrueOrFalseErr, MustBeUTCErr:
+				newVal = "must be specified"
+			default:
+				newVal = val
+			}
+
+			fields[newKey] = newVal
+		}
+
+		er.Error.Fields = fields
 	}
 
 	var data interface{}
